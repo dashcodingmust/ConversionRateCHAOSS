495929fd68294a0fe3cac5eacb62393fc9481089
Aditya Prasad <a.prasad.college@gmail.com>
2026-02-14 15:34:17 +0530

    Update Number_of_contributions.py

--- Number_of_contributions.py
+++ Number_of_contributions.py
@@ -3,24 +3,18 @@ import requests
 import pandas as pd
 
 #This function is to evaulate the level of investment for each contributor
-def investment(x): 
+def stage(x): 
   if x==1: return "D0"  #This is a first time commiter or contributor
   elif x>5: return "D2" #This is a regular commitor
   else: return "D1" 
+    
+def investment(owner,repo):
   
-owner = "" #insert file owner here
-repo = "" #insert repo name here
-
-url = f"https://api.github.com/repos/{owner}/{repo}/contributors" 
-
-response = requests.get(url)
-data = response.json()
-
-contributors = pd.DataFrame(data)[["login", "contributions"]]
-
-print(contributors.head()) #This will print out each contributors with their number of commits
-
-
-
-contributors["investment"]=contributors["contributions"].apply(investment)
-print(contributors.head())
+  url = f"https://api.github.com/repos/{owner}/{repo}/contributors" 
+  response = requests.get(url)
+  data = response.json()
+  
+  contributors = pd.DataFrame(data)[["login", "contributions"]]
+  print(contributors.head()) #This will print out each contributors with their number of commits
+  contributors["investment"]=contributors["contributions"].apply(investment)
+  print(contributors.head())
