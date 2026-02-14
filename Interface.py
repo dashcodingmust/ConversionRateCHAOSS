92784154fa363775960462003b7e87c636907281
aditya <a.prasad.college@gmail.com>
2026-02-14 15:46:03 +0530

    added an interface

---
+++ Interface.py
@@ -0,0 +1,6 @@
+#this is the interface
+import time_of_last_commit as t
+import Number_of_contributions as n
+
+n.investment(input('owner'),input('repo name'))
+t.lastCommitTime(input('number of commits'),input('owner'),input('repo name'))
