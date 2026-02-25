1b708637ccaa562c70834bc2d9cd940ab0e4be08
Aditya Prasad <a.prasad.college@gmail.com>
2026-02-25 12:56:28 +0530

    changed print statements to return pr_merge_rate.py

--- backend/src/pr_merge_rate.py
+++ backend/src/pr_merge_rate.py
@@ -19,17 +19,37 @@ def pr_merge_rate(owner, repo):
     total = len(pulls)
     rate = merged / total * 100
 
-    print("\n Pull Request Merge Report")
-    print("----------------------------")
-    print("Closed PRs analyzed:", total)
-    print("Merged PRs:", merged)
-    print("Merge Rate:", round(rate, 2), "%")
-
+    return {"Pull Request Merge Report",
+           "----------------------------",
+           "Closed PRs analyzed:": total,
+           "Merged PRs:": merged, 
+           "Merge Rate:", round(rate, 2), "%",
+            }
     if rate >= 80:
-        print("Status: Excellent ")
+        return {"Pull Request Merge Report",
+           "----------------------------",
+           "Closed PRs analyzed:": total,
+           "Merged PRs:": merged, 
+           "Merge Rate:", round(rate, 2), "%",
+            "Status: Excellent "}
     elif rate >= 50:
-        print("Status: Healthy ")
+        return {"Pull Request Merge Report",
+           "----------------------------",
+           "Closed PRs analyzed:": total,
+           "Merged PRs:": merged, 
+           "Merge Rate:", round(rate, 2), "%",
+            "Status: Healthy "}
     elif rate >= 30:
-        print("Status: Concerning ")
+        return {"Pull Request Merge Report",
+           "----------------------------",
+           "Closed PRs analyzed:": total,
+           "Merged PRs:": merged, 
+           "Merge Rate:", round(rate, 2), "%",
+            "Status: Concerning "}
     else:
-        print("Status: Risk 🚨")
+        return {"Pull Request Merge Report",
+           "----------------------------",
+           "Closed PRs analyzed:": total,
+           "Merged PRs:": merged, 
+           "Merge Rate:", round(rate, 2), "%",
+            "Status: Risk "}
