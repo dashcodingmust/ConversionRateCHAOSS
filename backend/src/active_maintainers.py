3a9e300db8ff0f03113f493ebc78ce14c76cf9b5
richaa2408 <richamonserate2408@gmail.com>
2026-03-06 23:01:26 +0530

    convert to async and changes to return distinct maintainer count

--- backend/src/active_maintainers.py
+++ backend/src/active_maintainers.py
@@ -1,48 +1,49 @@
-import requests
+import httpx
 from datetime import datetime, timedelta, timezone
 from config import HEADERS
 
 
-def active_maintainers(owner, repo, days=90):
+async def active_maintainers(owner, repo, days=90):
     page = 1
-    maintainers = 0
+    maintainers = set()
 
     cutoff = datetime.now(timezone.utc) - timedelta(days=days)
+    async with httpx.AsyncClient() as client:
 
-    while True:
-        url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-        params = {
-            "per_page": 100,
-            "page": page
-        }
+        while True:
+            url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+            params = {
+                "per_page": 100,
+                "page": page
+            }
 
-        response = requests.get(
-            url,
-            headers=HEADERS,
-            params=params,
-            timeout=10
-        )
-
-        if response.status_code != 200:
-            break
-
-        data = response.json()
-
-        if not data:
-            break
-
-        for commit in data:
-            commit_date_str = commit["commit"]["author"]["date"]
-            commit_date = datetime.fromisoformat(
-                commit_date_str.replace("Z", "+00:00")
+            response = await client.get(
+                url,
+                headers=HEADERS,
+                params=params,
+                timeout=10
             )
 
-            if commit_date < cutoff:
-                continue
+            if response.status_code != 200:
+                break
 
-            if commit.get("author") and commit["author"].get("login"):
-                maintainers += 1
+            data = response.json()
 
-        page += 1
+            if not data:
+                break
 
-    return maintainers
+            for commit in data:
+                commit_date_str = commit["commit"]["author"]["date"]
+                commit_date = datetime.fromisoformat(
+                    commit_date_str.replace("Z", "+00:00")
+                )
+
+                if commit_date < cutoff:
+                    continue
+
+                if commit.get("author") and commit["author"].get("login"):
+                    maintainers.add(commit["author"]["login"])
+
+            page += 1
+
+    return len(maintainers)
