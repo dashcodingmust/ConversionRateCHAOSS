6824416106ff2fdca993fd4e9a91ab9e4eb8ea2b
richaa2408 <richamonserate2408@gmail.com>
2026-03-06 11:36:46 +0530

    Update func call

--- Interface.py
+++ Interface.py
@@ -1,7 +1,7 @@
 from backend.src.number_of_contributions import investment
 from backend.src.time_of_last_commit import last_commit_time
 from backend.src.issue_backlog import issue_backlog
-from backend.src.pr_merge_rate import pr_merge_rate
+from backend.src.pr_merge_rate import pr_metrics
 from backend.src.commit_trend import commit_trend
 from backend.src.active_maintainers import active_maintainers
 
@@ -14,6 +14,6 @@ def analyze_repo(owner, repo, threshold):
     results["Commit Trend"] = commit_trend(owner, repo)
     results["Active Maintainers"] = active_maintainers(owner, repo)
     results["Issue Backlog"] = issue_backlog(owner, repo)
-    results["PR Merge Rate"] = pr_merge_rate(owner, repo)
+    results["PR Metrics"] = pr_metrics(owner, repo)
 
     return results
