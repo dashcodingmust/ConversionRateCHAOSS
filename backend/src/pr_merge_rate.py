2e96f6391b4c097d99cf97db2af5c5190792ebae
richaa2408 <richamonserate2408@gmail.com>
2026-03-06 11:34:11 +0530

    Add avg merge time

--- backend/src/pr_merge_rate.py
+++ backend/src/pr_merge_rate.py
@@ -1,11 +1,13 @@
 import requests
+from datetime import datetime
 from backend.src.config import HEADERS
 
 
-def pr_merge_rate(owner, repo):
+def pr_metrics(owner, repo):
     page = 1
     merged = 0
-    total = 0
+    total_closed = 0
+    total_merge_time_days = 0
 
     while True:
         url = f"https://api.github.com/repos/{owner}/{repo}/pulls"
@@ -22,11 +24,11 @@ def pr_merge_rate(owner, repo):
             timeout=10
         )
 
-        if response.status_code == 403:
-            return 0
-
         if response.status_code != 200:
-            return 0
+            return {
+                "merge_rate": 0,
+                "avg_merge_time_days": 0
+            }
 
         data = response.json()
 
@@ -34,13 +36,37 @@ def pr_merge_rate(owner, repo):
             break
 
         for pr in data:
-            total += 1
+            total_closed += 1
+
             if pr.get("merged_at") is not None:
                 merged += 1
 
+                created_at = datetime.fromisoformat(
+                    pr["created_at"].replace("Z", "+00:00")
+                )
+                merged_at = datetime.fromisoformat(
+                    pr["merged_at"].replace("Z", "+00:00")
+                )
+
+                merge_time = (merged_at - created_at).days
+                total_merge_time_days += merge_time
+
         page += 1
 
-    if total == 0:
-        return 0
+    if total_closed == 0:
+        return {
+            "merge_rate": 0,
+            "avg_merge_time_days": 0
+        }
 
-    return round((merged / total) * 100, 2)
+    merge_rate = round((merged / total_closed) * 100, 2)
+
+    avg_merge_time = (
+        round(total_merge_time_days / merged, 2)
+        if merged > 0 else 0
+    )
+
+    return {
+        "merge_rate": merge_rate,
+        "avg_merge_time_days": avg_merge_time
+    }
