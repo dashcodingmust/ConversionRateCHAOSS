50d043c504bf955f875ef908dc7e10f349642219
richaa2408 <richamonserate2408@gmail.com>
2026-03-06 23:16:59 +0530

    convert all files to async, removed limits, add days_since_last_commit

--- backend/src/time_of_last_commit.py
+++ backend/src/time_of_last_commit.py
@@ -1,17 +1,38 @@
-import requests
+import httpx
+from datetime import datetime, timezone
 from config import HEADERS
-def last_commit_time(limit, owner, repo):
+
+
+async def last_commit_time(owner, repo):
 
     url = f"https://api.github.com/repos/{owner}/{repo}/commits"
-    
 
-    response =requests.get(url, headers=HEADERS)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(
+            url,
+            headers=HEADERS,
+            timeout=10
+        )
+
+    if response.status_code != 200:
+        return {"status": "API error"}
 
     data = response.json()
 
     if not isinstance(data, list) or len(data) == 0:
-        return "No commit data found"
+        return {"status": "No commit data found"}
 
     last_commit = data[0]
+    commit_date_str = last_commit["commit"]["committer"]["date"]
 
-    return last_commit["commit"]["committer"]["date"]
+    commit_date = datetime.fromisoformat(
+        commit_date_str.replace("Z", "+00:00")
+    )
+
+    now = datetime.now(timezone.utc)
+    days_since = (now - commit_date).days
+
+    return {
+        "last_commit_date": commit_date_str,
+        "days_since_last_commit": days_since
+    }
