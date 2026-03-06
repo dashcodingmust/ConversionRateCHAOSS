6b754083e04ef9dd9a4e18f13d062f262f04fd23
richaa2408 <richamonserate2408@gmail.com>
2026-03-06 22:36:52 +0530

    new file for sending all git request in one place

---
+++ backend/src/github_client.py
@@ -0,0 +1,15 @@
+import httpx
+
+class GitHubClient:
+    def __init__(self, headers):
+        self.headers = headers
+
+    async def get(self, url, params=None):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(
+                url,
+                headers=self.headers,
+                params=params,
+                timeout=10
+            )
+            return response.json()
