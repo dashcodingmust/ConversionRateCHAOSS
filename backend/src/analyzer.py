9eb910c5ca410787d334fbb86695ba34e30e0470
richaa2408 <richamonserate2408@gmail.com>
2026-03-07 00:42:52 +0530

    add pr_backlog function to matrix

--- backend/src/analyzer.py
+++ backend/src/analyzer.py
@@ -1,7 +1,7 @@
 import asyncio
 from number_of_contributions import investment
 from commit_trend import commit_trend
-from pr_merge_rate import pr_metrics
+from pr_merge_rate import pr_metrics, pr_backlog
 from issue_backlog import issue_backlog
 from active_maintainers import active_maintainers
 from time_of_last_commit import last_commit_time
@@ -11,6 +11,7 @@ async def analyze_repo(owner, repo, threshold):
         investment(owner, repo, threshold),
         commit_trend(owner, repo),
         pr_metrics(owner, repo),
+        pr_backlog(owner, repo),
         issue_backlog(owner, repo),
         active_maintainers(owner, repo),
         last_commit_time(owner, repo)
@@ -23,6 +24,8 @@ def combine(results):
         "Contributor Engagement": results[0],
         "Commit Trend": results[1],
         "PR Metrics": results[2],
-        "Issue Backlog": results[3],
-        "Active Maintainers": results[4],
+        "PR Backlog": results[3],
+        "Issue Backlog": results[4],
+        "Active Maintainers": results[5],
+        "Last Commit Time": results[6]
     }
